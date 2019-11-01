cf9bd1e35a39e6b6dd82baec58ad534067eccf73
Ashwini Manoj <ashwinimanoj@gmail.com>
2019-11-01 18:38:20 +0530

    Update from using python2 to python3, clean up code

--- findSeq.py
+++ findSeq.py
@@ -7,14 +7,15 @@ first multiplying by 3 and then adding 5 twice, whereas the number 15
 cannot be reached at all.'''
 
 
+def findSeq(start, history, target) -> str:
+    if start == target:
+        return f'({history} = {str(target)})'
+    elif start > target:
+        return 0
+    else:
+        return findSeq(5 + start, f'({history} + 5)', target)\
+            or findSeq(3 * start, f'({history} * 3)', target)
 
-def findSeq(start,history,target):
-	if start==target:
-		return history+"="+str(target)
-	elif start>target:
-		return 0
-	else:
-		return findSeq(5+start,"("+history+"+ 5)",target) or findSeq(3*start,"("+history+"*3)",target)
 
-num=int(raw_input("Enter number: "))
-print findSeq(1,"1",num) 
+num = int(input("Enter number: "))
+print(findSeq(1, "1", num))
